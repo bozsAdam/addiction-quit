025f38f7853863acfdd099261b87eb4356a076b2
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-22 14:04:32 +0200

    Add CalculatorService with blank calculateMoneySince method

---
+++ src/main/java/hu/adam/nosmoke/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package hu.adam.nosmoke.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class CalculatorService {
+
+    public long calculateMoneySince(LocalDate date, long cost){
+        return -1;
+    }
+}
