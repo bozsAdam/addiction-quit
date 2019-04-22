212d1e804d07c96e3f3bce1515e0020ead857d47
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-22 14:28:16 +0200

    Add ItemService with blank CRUD methods

---
+++ src/main/java/hu/adam/nosmoke/service/ItemService.java
@@ -0,0 +1,18 @@
+package hu.adam.nosmoke.service;
+
+import hu.adam.nosmoke.model.Item;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    public void saveItem(Item item){}
+
+    public Item getItem(long id){return null;}
+
+    public void updateItem(long id){}
+
+    public void deleteItem(long id){}
+
+
+}
