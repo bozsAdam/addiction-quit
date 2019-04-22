e6442a331878d102a9078e74f0a933b29a6d9c52
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-22 14:29:00 +0200

    Add dependency injection to ItemService

--- src/main/java/hu/adam/nosmoke/service/ItemService.java
+++ src/main/java/hu/adam/nosmoke/service/ItemService.java
@@ -1,10 +1,15 @@
 package hu.adam.nosmoke.service;
 
 import hu.adam.nosmoke.model.Item;
+import hu.adam.nosmoke.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
 
     public void saveItem(Item item){}
 
