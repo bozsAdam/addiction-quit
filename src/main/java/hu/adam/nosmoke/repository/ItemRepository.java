8b507adbccae9594f9b3511cb492fafb389ffd23
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-22 14:08:39 +0200

    Add ItemRepository

---
+++ src/main/java/hu/adam/nosmoke/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package hu.adam.nosmoke.repository;
+
+import hu.adam.nosmoke.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Long> {
+}
