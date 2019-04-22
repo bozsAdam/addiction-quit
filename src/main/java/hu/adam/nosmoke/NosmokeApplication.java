0343d131fb0c30324ecd1a017bccd913eebf1898
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-22 13:44:05 +0200

    Initial commit

---
+++ src/main/java/hu/adam/nosmoke/NosmokeApplication.java
@@ -0,0 +1,13 @@
+package hu.adam.nosmoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosmokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosmokeApplication.class, args);
+    }
+
+}
