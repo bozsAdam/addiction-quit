369ac6f91090467f40f8ec75f156fe321ad99ce5
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-23 13:13:25 +0200

    Add methodbody for calculateMoneySince with logging

--- src/main/java/hu/adam/nosmoke/service/CalculatorService.java
+++ src/main/java/hu/adam/nosmoke/service/CalculatorService.java
@@ -1,13 +1,23 @@
 package hu.adam.nosmoke.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 import java.time.LocalDate;
 
 @Service
+@Slf4j
 public class CalculatorService {
 
     public float calculateMoneySince(LocalDate date, float cost){
-        return -1f;
+        if(cost < 0){
+            log.error("Cost is smaller than 0");
+            throw new IllegalArgumentException();
+        }
+
+        log.info("Returning the calculated data");
+        return DAYS.between(date,LocalDate.now()) * cost;
+
     }
 }
