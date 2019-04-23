6354dfd62f03831575eb6b971b9aca48aef65cab
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-23 15:24:35 +0200

    Add getItem endpoint

--- src/main/java/hu/adam/nosmoke/controller/ItemController.java
+++ src/main/java/hu/adam/nosmoke/controller/ItemController.java
@@ -5,10 +5,7 @@ import hu.adam.nosmoke.repository.ItemRepository;
 import hu.adam.nosmoke.service.ItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +25,10 @@ public class ItemController {
     public List<Item> getItems(){
         return itemService.getItems();
     }
+
+    @GetMapping("/item/{id}")
+    public Item getItem(@PathVariable long id){
+        return itemService.getItem(id);
+    }
+
 }
