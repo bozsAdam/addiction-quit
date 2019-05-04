4429e3acf94216498371f583e65142dc2c3a0401
Adam Bozsoki <09.adam.b@gmail.com>
2019-05-04 16:04:19 +0200

    Add main design

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems);
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
 });
