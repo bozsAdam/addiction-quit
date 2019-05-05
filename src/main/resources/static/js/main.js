26c09757f7e22fe7b8e18b28f9301952e2fb9869
Adam Bozsoki <09.adam.b@gmail.com>
2019-05-05 16:11:26 +0200

    Add months object and getDate function

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,4 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
 });
 
+let months = {
+    "Jan":"01",
+    "Feb":"02",
+    "Mar":"03",
+    "Apr":"04",
+    "May":"05",
+    "Jun":"06",
+    "Jul":"07",
+    "Aug":"08",
+    "Sep":"09",
+    "Oct":"10",
+    "Nov":"11",
+    "Dec":"12"
+};
 
+let getDate = ()=>{
+    let date = document.getElementById("stopdate")
+                        .value
+                        .replace(",","")
+                        .split(" ");
+
+    return date[2] + "-" + months[date[0]] + "-" + date[1];
+};
